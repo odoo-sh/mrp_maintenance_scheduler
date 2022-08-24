8b076657c2f75542a7effe4c8476155f2e65897f
Sowrirajan, Sodexis <sowrirajan@sodexis.com>
2022-08-24 18:58:02 +0530

    [ID: 25180 ] Design for Maintenance Flow:-Correction

--- mrp_maintenance_scheduler/models/maintenance_equipment.py
+++ mrp_maintenance_scheduler/models/maintenance_equipment.py
@@ -17,18 +17,12 @@ class MaintenanceEquipmentStep(models.Model):
     
     maintenance_equipment_id = fields.Many2one("maintenance.equipment")
     maintenance_operation_id = fields.Many2one('maintenance.operations',string="Maintenance Operation")
-    technician_user_id = fields.Many2one('res.users', string='Assigned to', tracking=True)
-    maintenance_team_id = fields.Many2one('maintenance.team', string='Maintenance Team',required=True)
-    maintenance_duration = fields.Float(help="Maintenance Duration in hours.")
     next_action_date = fields.Date(compute='_compute_next_maintenance',store=True,string='Next Preventive Maintenance')
-    period = fields.Integer(string='Preventive Maintenance Frequency')
-    
-    
-    
-    @api.depends('maintenance_equipment_id.effective_date', 'period', 'maintenance_equipment_id.maintenance_ids.request_date', 'maintenance_equipment_id.maintenance_ids.close_date')
+
+    @api.depends('maintenance_equipment_id.effective_date', 'maintenance_operation_id.period', 'maintenance_equipment_id.maintenance_ids.request_date', 'maintenance_equipment_id.maintenance_ids.close_date')
     def _compute_next_maintenance(self):
         date_now = fields.Date.context_today(self)
-        equipments = self.filtered(lambda x: x.period > 0)
+        equipments = self.filtered(lambda x: x.maintenance_operation_id.period > 0)
         for equipment in equipments:
             next_maintenance_todo = self.env['maintenance.request'].search([
                 ('equipment_id', '=', equipment.maintenance_equipment_id.id),
@@ -45,26 +39,26 @@ class MaintenanceEquipmentStep(models.Model):
                 date_gap = next_maintenance_todo.request_date - last_maintenance_done.close_date
                 # If the gap between the last_maintenance_done and the next_maintenance_todo one is bigger than 2 times the period and next request is in the future
                 # We use 2 times the period to avoid creation too closed request from a manually one created
-                if date_gap > timedelta(0) and date_gap > timedelta(days=equipment.period) * 2 and next_maintenance_todo.request_date > date_now:
+                if date_gap > timedelta(0) and date_gap > timedelta(days=equipment.maintenance_operation_id.period) * 2 and next_maintenance_todo.request_date > date_now:
                     # If the new date still in the past, we set it for today
-                    if last_maintenance_done.close_date + timedelta(days=equipment.period) < date_now:
+                    if last_maintenance_done.close_date + timedelta(days=equipment.maintenance_operation_id.period) < date_now:
                         next_date = date_now
                     else:
-                        next_date = last_maintenance_done.close_date + timedelta(days=equipment.period)
+                        next_date = last_maintenance_done.close_date + timedelta(days=equipment.maintenance_operation_id.period)
             elif next_maintenance_todo:
                 next_date = next_maintenance_todo.request_date
                 date_gap = next_maintenance_todo.request_date - date_now
                 # If next maintenance to do is in the future, and in more than 2 times the period, we insert an new request
                 # We use 2 times the period to avoid creation too closed request from a manually one created
-                if date_gap > timedelta(0) and date_gap > timedelta(days=equipment.period) * 2:
-                    next_date = date_now + timedelta(days=equipment.period)
+                if date_gap > timedelta(0) and date_gap > timedelta(days=equipment.maintenance_operation_id.period) * 2:
+                    next_date = date_now + timedelta(days=equipment.maintenance_operation_id.period)
             elif last_maintenance_done:
-                next_date = last_maintenance_done.close_date + timedelta(days=equipment.period)
+                next_date = last_maintenance_done.close_date + timedelta(days=equipment.maintenance_operation_id.period)
                 # If when we add the period to the last maintenance done and we still in past, we plan it for today
                 if next_date < date_now:
                     next_date = date_now
             else:
-                next_date = equipment.maintenance_equipment_id.effective_date + timedelta(days=equipment.period)
+                next_date = equipment.maintenance_equipment_id.effective_date + timedelta(days=equipment.maintenance_operation_id.period)
             equipment.next_action_date = next_date
         (self - equipments).next_action_date = False
     
--- mrp_maintenance_scheduler/models/maitenance_operation.py
+++ mrp_maintenance_scheduler/models/maitenance_operation.py
@@ -7,18 +7,22 @@ from odoo import models, fields, api
 class MaintenanceOperations(models.Model):
     _name = "maintenance.operations"
     _description = "Maintenance Operations"
+    _inherit = ['mail.thread']
     
     name = fields.Char(string="Name")
+    period = fields.Integer(string='Preventive Maintenance Frequency')
+    maintenance_duration = fields.Float(help="Maintenance Duration in hours.")
+    technician_user_id = fields.Many2one('res.users', string='Assigned to', tracking=True)
+    maintenance_team_id = fields.Many2one('maintenance.team', string='Maintenance Team',required=True)
     line_ids = fields.One2many("maintenance.operations.step","operation_id",string="Steps")
-    
-    
-    
+
+
 class MaintenanceOperationsLine(models.Model):
     _name = "maintenance.operations.step"
     _description = "Maintenance Operations Step"
     _rec_name = "step_name"
-    
+
     step_name = fields.Char(string="Step Name")
-    duration_expected = fields.Float(string="Expected Duration")
+    duration_expected = fields.Float(string="Expected Duration",required=True)
     procedure = fields.Html("Procedure")
     operation_id = fields.Many2one("maintenance.operations")
